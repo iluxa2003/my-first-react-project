6136230967299fee68a2985c485c6ab07311dc41
iluxa2003 <ilya.yakov.com@gmail.com>
2022-08-13 13:17:56 +0300

    My first react component!

--- my/src/App.js
+++ my/src/App.js
@@ -1,7 +1,12 @@
+import Expenseitem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <Expenseitem></Expenseitem>
+      <Expenseitem></Expenseitem>
+      <Expenseitem></Expenseitem>
     </div>
   );
 }
---
+++ my/src/components/ExpenseItem.js
@@ -0,0 +1,6 @@
+function Expenseitem() {
+    return (
+        <h2>Expence Item here!!!</h2>
+    );
+}
+export default Expenseitem;
