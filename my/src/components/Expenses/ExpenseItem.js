fa7c81bd8f1b5efd90b6b4aec388f42b5b1cf6e5
iluxa2003 <ilya.yakov.com@gmail.com>
2022-08-14 14:33:07 +0300

    starting with events

--- my/src/components/Expenses/ExpenseItem.js
+++ my/src/components/Expenses/ExpenseItem.js
@@ -2,17 +2,20 @@ import React from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
+
 function ExpenseItem(props) {
-
-
+  const clickHandler = () =>{
+    console.log("click")
+  }
 
   return (
     <Card className="expense-item">
         <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div className="expense-item__price">{props.amount}</div>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 }
